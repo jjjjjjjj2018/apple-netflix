16a1510f49d7f7009bcf6786fa7c61723734354e
Jason Yu <jasonyu1994@gmail.com>
2019-12-17 18:39:06 -0800

    change to function comp

--- netflix/src/Components/App.js
+++ netflix/src/Components/App.js
@@ -4,52 +4,49 @@ import { getAll, removeFromList, moveToMyList } from '../redux/actions';
 import List from './List';
 import './App.css';
 
-class App extends React.Component {
+const App = (props) => {
 
-  componentDidMount() {
-    this.props.getAll();
-  }
+  useEffect(() => {
+    props.getAll();
+  }, []);
+  const { mylist, recommendations, error, isLoading } = props;
 
-  render() {
-
-    const { mylist, recommendations, error, isLoading } = this.props;
-
-    return (
-      <div className="container" >
-        <div className='title-bar-container'>
-          <img className='title-img' alt='logo' src='https://assets.brand.microsites.netflix.io/assets/1ed15bca-b389-11e7-9274-06c476b5c346_cm_800w.png?v=21' />
-        </div>
-        {error && <div>Oopps</div>}
-        {!error && !isLoading &&
-          <div >
-            <List
-              name='My List'
-              list={mylist || []}
-              click={this.props.removeFromList}
-              btnName='Remove'
-            />
-            <List
-              name='Recommendations'
-              list={recommendations || []}
-              click={this.props.moveToMyList}
-              btnName='Add to MyList'
-            />
-            <div className='list-title-container'>
-              {mylist &&
-                <ul>{mylist.map(item => {
-                  return (
-                    <li key={item.id} >
-                      <font color='white'>{item.title}</font>
-                    </li>
-                  )
-                })}</ul>
-              }
-            </div>
-          </div>
-        }
+  return (
+    <div className="container" >
+      <div className='title-bar-container'>
+        <img className='title-img' alt='logo' src='https://assets.brand.microsites.netflix.io/assets/1ed15bca-b389-11e7-9274-06c476b5c346_cm_800w.png?v=21' />
       </div>
-    );
-  }
+      {error && <div>Oopps</div>}
+      {!error && !isLoading &&
+        <div >
+          <List
+            name='My List'
+            list={mylist || []}
+            click={props.removeFromList}
+            btnName='Remove'
+          />
+          <List
+            name='Recommendations'
+            list={recommendations || []}
+            click={props.moveToMyList}
+            btnName='Add to MyList'
+          />
+          <div className='list-title-container'>
+            {mylist &&
+              <ul>{mylist.map(item => {
+                return (
+                  <li key={item.id} >
+                    <font color='white'>{item.title}</font>
+                  </li>
+                )
+              })}</ul>
+            }
+          </div>
+        </div>
+      }
+    </div>
+  );
+
 }
 
 const mapStateToProps = ({ mylist, recommendations, error, isLoading }) => ({ mylist, recommendations, error, isLoading })
