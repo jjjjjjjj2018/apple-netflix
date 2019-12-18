e991e30bb717b152b2c7783abbf0ea11a5ba20df
Jason Yu <jasonyu1994@gmail.com>
2019-12-17 18:53:49 -0800

    change import

--- netflix/src/Components/App.js
+++ netflix/src/Components/App.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getAll, removeFromList, moveToMyList } from '../redux/actions';
 import List from './List';
